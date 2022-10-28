c1c4c1c8020d5a39082c311fbb0adcd0c187a7f3
HunterBjj <a.urmanow@mail.ru>
2022-10-28 23:03:02 +0700

    2PR

---
+++ Pr2/2.2.py
@@ -0,0 +1,10 @@
+from tokenize import Double
+
+
+a = int(input("Введите чило a:"))
+b = int(input("Введите число b:"))
+arg1 = a%b
+arg2 = a//b # не стал приводить к float, т.к. целочисленное деление
+print(f"Остаток от деления = {arg1} \nЦелочисленное деление = {arg2}")
+
+
