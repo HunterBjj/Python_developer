f973c20def20ba2e26e483d0481e88a4652ce441
Bjj <a.urmanow@mail.ru>
2023-01-26 14:10:55 +0700

    Выбран другой алгоритм

--- Pr8/8.3.py
+++ Pr8/8.3.py
@@ -8,19 +8,21 @@
 import time
 start_time = time.perf_counter()
 sentences = ['капитан джек воробей, капитан дальнего плавания,ваша лодка готова, капитан']
-sentences_split = sentences.split()
-sentences_count = sentences_split.count()
-print(sentences_count.count('капитан'))
+result = sentences[0].count('капитан')
+print(result)
 end_time = time.perf_counter()
 print(f"Время после рефакторинга: {end_time - start_time:.8f} seconds")
 #Итератор хранит только указатель на следующий элемент,мы всегда храним только один элемент в памяти, а не всю коллекцию, поэтому использовать генератор в данном случаи быстрее быстрее
 start_time1 = time.perf_counter()
-
-cap_count1 = 0
+sentences = ['капитан джек воробей',
+             'капитан дальнего плавания',
+             'ваша лодка готова, капитан']
+cap_count = 0
 for sentence in sentences:
-    cap_count1 += sentence.count('капитан')
+    cap_count += sentence.count('капитан')
 
-print(cap_count1)
+print(cap_count)
+
 end_time1 = time.perf_counter()
 print(f" Время до рефакторинга: {end_time1 - start_time1:.8f} seconds")
 
