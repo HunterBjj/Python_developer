9e08b6257bf65fc10866284a22c3378df3817d9b
Bjj <a.urmanow@mail.ru>
2023-02-18 13:47:41 +0700

    update alghoritm function

--- Pr8/8.6.py
+++ Pr8/8.6.py
@@ -9,19 +9,28 @@
     largest_histogram([1, 1, 3, 1]) == 4
     largest_histogram([2, 1, 4, 5, 1, 3, 3]) == 8
 """
-num = input("Введите высоту гистограммы ").split()
-len_num = len(num)
-height = []
-size = 0
-minnum = 1
-for i in range(1, len_num, 1):
-    if int(num[i - 1]) <= int(num[i]):
-        size += 1
-        minnum = int(num[i - 1])
-    else:
-        result = int(num[i - 1]) * size
-        #result = minnum * size
-        height.append(result)
-        size = 1
 
-print(height)
+
+def square(int_num):  # К сожалению, не обошлось без большой вложенности в данной функции
+    int_num.append(0)
+    result_n = []
+    len_num = len(int_num)
+    for i in range(len_num):
+        size = 1
+        for g in range(i + 1, len_num, 1):
+            if int_num[i] <= int_num[g]:
+                size += 1
+            else:
+                if int_num[i] < int_num[i - 1]:
+                    size += 1
+                result_next = int_num[i] * size
+                result_n.append(result_next)
+                break
+    max_sq = max(result_n)
+    return max_sq
+
+
+num = input("Введите высоту гистограммы ").split()
+int_num = list(map(int, num))
+max_square = square(int_num)
+print(f' {num} max square: {max_square} ')
---
+++ Pr9/9.3.py
@@ -0,0 +1,12 @@
+"""
+3. Рефакторинг.
+Дано: неоптимальный код.
+Задание: оптимизировать следующий код.
+def responses_creator(item_ids):
+    item_ids = [None] if item_ids is None else item_ids
+    responses = []
+    for item_id in item_ids:
+        new_response = dict(item_id=item_id)
+        responses.append(new_response)
+    return responses
+"""
