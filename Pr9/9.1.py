02ce7e9c8c7a859effdb01b83b8b0fc9b7dbd5f5
Bjj <a.urmanow@mail.ru>
2023-02-19 12:14:06 +0700

    update alghoritm function

--- Pr9/9.1.py
+++ Pr9/9.1.py
@@ -6,3 +6,14 @@
 Пример:
  list(f(3)), результат: [0.460552766096591, 2.6440795402868016, 0.3830553232366699]
 """
+import random
+
+
+def random_num(count_num):
+    for i in range(1, count_num + 1, 1):
+        yield random.uniform(0, 5)
+
+
+count_num = int(input("Введите количество сгенерированых чисел "))
+result = list(random_num(count_num))
+print(result)
