257bd1ef50ac3dfb1315d2df44a3816f082cd226
Bjj <a.urmanow@mail.ru>
2023-02-19 16:01:57 +0700

    update alghoritm function

--- Pr9/9.2.py
+++ Pr9/9.2.py
@@ -5,3 +5,17 @@
 Пример:
 list(f([1, 2], [3, 4])), результат: [1, 2, 3, 4]
 """
+from itertools import chain
+
+
+def union_num(num1, num2):
+    result = list(chain(num1, num2)) #Создаёт итератор представляющий единой цепочкой элементы указанных объектов.
+    return result
+
+
+num1 = input("Enter number1 ").split()
+int_num1 = list(map(int, num1))
+num2 = input("Enter number2 ").split()
+int_num2 = list(map(int, num2))
+res = union_num(int_num1, int_num2)
+print(res)
