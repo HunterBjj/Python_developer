554a0a6d0176eb295ec1526c565510f46d39db4f
Bjj <a.urmanow@mail.ru>
2023-03-25 15:47:42 +0700

    OOP

---
+++ Pr10/1.py
@@ -0,0 +1,39 @@
+"""1. Друзья.
+Дано: n - целое число.
+Задание:
+Создайте класс "Friends", который должен содержать данные о людях (их имена)
+и о связях между ними. Имена представлены в виде текстовых строк,
+чувствительных к регистру. Связи не имеют направлений, то есть,
+если существует связь "sofia" с "nikola", это справедливо и в обратную сторону.
+"""
+
+
+class Friends:
+
+    def __init__(self, connection):
+        self.connection = connection
+
+    def add_set(self, element=None):
+        if not (element in self.connection):
+            self.connection.append(element)
+            return True
+        return False
+
+    def remove_set(self, element=None):
+        if element in self.connection:
+            self.connection.remove(element)
+            return True
+        return False
+
+    def names_set(self, element=None):
+        return False
+
+    def connected_set(self, element=None):
+        if element in self.connection:
+
+        return set()
+
+
+f = Friends([{"1", "2"}, {"3", "1"}])
+result = f.add({"2", "1"})
+print(result)
--- Pr9/9.3.py
+++ Pr9/9.3.py
@@ -12,9 +12,13 @@ def responses_creator(item_ids):
 """
 
 def responses_creator(item_ids):
-    item_ids = [None] if item_ids is None else item_ids
-    responses = []
-    for item_id in item_ids:
-        new_response = dict(item_id=item_id)
-        responses.append(new_response)
+    if item_ids is None:
+        return None
+    else:
+        responses = []
+        for item_id in item_ids:
+            new_response = dict(item_id=item_id)
+            responses.append(new_response)
     return responses
+
+responses_creator(item_ids= 12)
