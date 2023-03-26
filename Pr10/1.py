69df5d2dc21ddb97074f33e5e80f8a4adab980fb
Bjj <a.urmanow@mail.ru>
2023-03-26 13:27:56 +0700

    upgrade Method

--- Pr10/1.py
+++ Pr10/1.py
@@ -28,11 +28,23 @@ class Friends:
     def names_set(self, element=None):
         return False
 
-    def connected_set(self, element=None):
-        for i in range()
-        return set()
+    def connected_set(self, element=None):  # Виталий Александрович, подскадите, пожалуйста, как решить боллее эффективно этот метод, а то плохая ассимптотическая сложность
+        connected_res = []
+        connect = self.connection
+        for i in range(len(connect)):
+            if connect[i].isdisjoint(element) == False:
+                connected_res.append(connect[i])
+        if not connected_res:
+            return set()
+        for i in range(len(connected_res)):
+            connected_res[i].remove(element)
+        return connected_res
 
 
 f = Friends([{"1", "2"}, {"3", "1"}])
-result = f.add({"2", "1"})
+result = f.add_set({"2", "1"})
 print(result)
+result2 = f.connected_set("1")
+print(result2)
+
+
