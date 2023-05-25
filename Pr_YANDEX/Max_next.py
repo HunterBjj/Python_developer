1dcafb2bac4367c57e5f6d8cfddf5bb8d0b67b27
Bjj <a.urmanow@mail.ru>
2023-05-25 10:43:55 +0700

    add

---
+++ Pr_YANDEX/Max_next.py
@@ -0,0 +1,22 @@
+"""
+Дана последовательность чисел длинной N (N > 1)
+Найти максимальное число в последовательности и второе по величине число
+(такое, которое будет максимальным, если вычеркнуть из последовательности одно максимальное число)
+"""
+
+
+def max_max2(seq):
+    max_num = 0
+    next_max = 0
+    for i in range(len(seq)):
+        if seq[i] > max_num:
+            next_max = max_num
+            max_num = seq[i]
+        elif next_max < seq[i]:
+            next_max = seq[i]
+
+    return max_num, next_max
+
+seq = [1, 2, 3, 4, 5, 6, 7 , 22, 145, 1]
+result = max_max2(seq)
+print(result)
